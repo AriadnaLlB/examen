2c6a4e43f324cb8fce81565eafbf965fc182e064
AriadnaLlB <ariadna.lb@outlook.com>
2023-07-19 20:47:31 +0200

    add ejercicios

--- ejercicio1/ejercicio.sql
+++ ejercicio1/ejercicio.sql
@@ -1,5 +1,5 @@
 SELECT id FROM usuarios WHERE nombre = 'Juan';
 
-INSERT INTO usuarios (nombre, apellido, correo) VALUES ('Juan', 'Apellido', 'correo@ejemplo.com');
+INSERT INTO usuarios (nombre, apellido, correo) VALUES ('Juan', 'Rodriguez', 'juan@correo.com');
 
 UPDATE usuarios SET edad = 60 WHERE nombre = 'Juan';
